b8531aa5f0badfde9532d16e11ea833ff618d72c
taramakhija51 <tmakhija@uchicago.edu>
2022-10-18 06:16:26 +0000

    everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,16 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    render({ :template => "game_templates/user_rock.html.erb"})
+  end
+  def play_paper
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb"})
+  end
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
 end
